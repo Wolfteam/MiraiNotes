3a8b29d4b7a7c1ec4758ec8deee73acf7f3d3ab6
Efrain Bastidas <ebastidas@smatersolutions.com.ve>
2018-07-17 08:30:08 -0400

    [UWP] Added a Google Task Status

---
+++ MiraiNotes.UWP/Helpers/GoogleTaskStatusHelper.cs
@@ -0,0 +1,26 @@
+﻿using MiraiNotes.UWP.Models;
+using System;
+
+namespace MiraiNotes.UWP.Helpers
+{
+    public static class GoogleTaskStatusHelper
+    {
+        /// <summary>
+        /// Gets the google status string associated to <paramref name="status"/>
+        /// </summary>
+        /// <param name="status"></param>
+        /// <returns>Returns the google status string</returns>
+        public static string GetString(this GoogleTaskStatus status)
+        {
+            switch (status)
+            {
+                case GoogleTaskStatus.NEEDS_ACTION:
+                    return "needsAction";
+                case GoogleTaskStatus.COMPLETED:
+                    return "completed";
+                default:
+                    throw new ArgumentNullException($"The provided status {status} doesnt exists");
+            }
+        }
+    }
+}
---
+++ MiraiNotes.UWP/Models/GoogleTaskStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace MiraiNotes.UWP.Models
+{
+    public enum GoogleTaskStatus
+    {
+        NEEDS_ACTION = 0,
+        COMPLETED = 1
+    }
+}
