7dbdfeb9eb95ae0cb3c9dc0bceafea27c2e88c68
Efrain Bastidas <ebastidas@smatersolutions.com.ve>
2018-08-19 17:01:18 -0400

    [Shared] Added a shared project that will contain common logic / models

---
+++ MiraiNotes.Shared/Models/GoogleTaskStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace MiraiNotes.Shared.Models
+{
+    public enum GoogleTaskStatus
+    {
+        NEEDS_ACTION = 0,
+        COMPLETED = 1
+    }
+}
---
+++ MiraiNotes.Shared/Models/Result.cs
@@ -0,0 +1,8 @@
+﻿namespace MiraiNotes.Shared.Models
+{
+    public class Result
+    {
+        public bool Succeed { get; set; }
+        public string Message { get; set; }
+    }
+}
