14ac378c5319b0c9a9f3696885e452a9cd05c628
Efrain Bastidas <ebastidas@smatersolutions.com.ve>
2018-09-10 17:22:04 -0400

    [UWP] Added a initial logic for the intervals that a  bg tasks runs

--- MiraiNotes.UWP/Helpers/BackgroundTasksManager.cs
+++ MiraiNotes.UWP/Helpers/BackgroundTasksManager.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.Toolkit.Uwp.Helpers;
 using MiraiNotes.UWP.BackgroundTasks;
+using MiraiNotes.UWP.Models;
+using MiraiNotes.UWP.Services;
+using System;
 using Windows.ApplicationModel.Background;
 
 namespace MiraiNotes.UWP.Helpers
 {
     public class BackgroundTasksManager
     {
+        //TODO: NOT SURE IF I SHOULD REGISTER ALL MY BG TASKS AT THE SAME TIME
         public static void RegisterBackgroundTasks()
         {
             if (BackgroundTaskHelper.IsBackgroundTaskRegistered(typeof(SyncBackgroundTask)))
@@ -14,19 +18,38 @@ namespace MiraiNotes.UWP.Helpers
                 return;
             }
 
+            //TODO: CHANGE THIS WHEN YOU IMPLEMENT APP SETTINGS 
+            int syncBackgroundTaskInterval = ApplicationSettingsService.SyncBackgroundTaskInterval;
+            if (syncBackgroundTaskInterval <= 0)
+                return;
+
             //TODO: CHANGE THE DEFAULT TIME THAT THE BGTask WILL GET EXECUTED
             BackgroundTaskHelper.Register(
                 nameof(SyncBackgroundTask),
-                new TimeTrigger(15, false),
+                new TimeTrigger((uint)syncBackgroundTaskInterval, false),
                 false,
                 true,
                 new SystemCondition(SystemConditionType.FreeNetworkAvailable),
                 new SystemCondition(SystemConditionType.InternetAvailable));
         }
 
-        public static void UnregisterBackgroundTasks()
+        /// <summary>
+        /// Unregister a background task specified by <paramref name="backgroundTask"/>
+        /// </summary>
+        /// <param name="backgroundTask">The background task to unregister</param>
+        public static void UnregisterBackgroundTasks(BackgroundTaskType backgroundTask = BackgroundTaskType.ANY)
         {
-            BackgroundTaskHelper.Unregister(nameof(SyncBackgroundTask));
+            switch (backgroundTask)
+            {
+                case BackgroundTaskType.ANY:
+                    BackgroundTaskHelper.Unregister(nameof(SyncBackgroundTask));
+                    break;
+                case BackgroundTaskType.SYNC:
+                    BackgroundTaskHelper.Unregister(nameof(SyncBackgroundTask));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"The provided BackgroundTaskType doesnt exists {backgroundTask}");
+            }
         }
     }
 }
---
+++ MiraiNotes.UWP/Services/ApplicationSettingsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace MiraiNotes.UWP.Services
+{
+    public class ApplicationSettingsService
+    {
+        //TODO: I SHOULD ADD A ENUM FOR THE VALUES
+        public static int SyncBackgroundTaskInterval
+        {
+            get => (int)(ApplicationData.Current.LocalSettings.Values[nameof(SyncBackgroundTaskInterval)] ?? 0);
+            set => ApplicationData.Current.LocalSettings.Values[nameof(SyncBackgroundTaskInterval)] = value;
+        }
+    }
+}
