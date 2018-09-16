c652bb3788bfdb714d8f059238316c2e9e08ca75
Efrain Bastidas <ebastidas@smatersolutions.com.ve>
2018-09-15 23:30:01 -0400

    [UWP] Settings page vm improvemtns

--- MiraiNotes.UWP/ViewModels/SettingsPageViewModel.cs
+++ MiraiNotes.UWP/ViewModels/SettingsPageViewModel.cs
@@ -3,7 +3,9 @@ using GalaSoft.MvvmLight.Command;
 using MiraiNotes.UWP.Delegates;
 using MiraiNotes.UWP.Interfaces;
 using MiraiNotes.UWP.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 
 namespace MiraiNotes.UWP.ViewModels
@@ -12,16 +14,24 @@ namespace MiraiNotes.UWP.ViewModels
     {
         #region Members
         private readonly IApplicationSettingsService _appSettings;
+        private readonly ICustomDialogService _dialogService;
 
         private bool _isBackButtonVisible;
+        private string _currentPageText;
         #endregion
 
         #region Events
-        public SettingsNavigationRequest NavigationRequest;
+        public event SettingsNavigationRequest NavigationRequest;
         #endregion
 
 
         #region Properties
+        public string CurrentPageText
+        {
+            get => _currentPageText;
+            set => Set(ref _currentPageText, value);
+        }
+
         public List<SettingsPageItem> SettingsPages { get; } = new List<SettingsPageItem>
         {
             new SettingsPageItem
@@ -55,45 +65,141 @@ namespace MiraiNotes.UWP.ViewModels
             get => _isBackButtonVisible;
             set => Set(ref _isBackButtonVisible, value);
         }
+
+        public List<ItemModel> TasksSortTypes { get; } = new List<ItemModel>
+        {
+            new ItemModel
+            {
+                ItemID = TaskSortType.BY_NAME_ASC.ToString(),
+                Text = "By name asc"
+            },
+            new ItemModel
+            {
+                ItemID = TaskSortType.BY_NAME_DESC.ToString(),
+                Text = "By name desc"
+            },
+            new ItemModel
+            {
+                ItemID = TaskSortType.BY_UPDATED_DATE_ASC.ToString(),
+                Text = "By updated date asc"
+            },
+            new ItemModel
+            {
+                ItemID = TaskSortType.BY_UPDATED_DATE_DESC.ToString(),
+                Text = "By updated date desc"
+            },
+        };
+
+        public List<ItemModel> SyncBgTaskIntervalTypes { get; } = new List<ItemModel>
+        {
+            new ItemModel
+            {
+                ItemID = SyncBgTaskIntervals.NEVER.ToString(),
+                Text = "Never"
+            },
+            new ItemModel
+            {
+                ItemID = SyncBgTaskIntervals.EACH_3_HOURS.ToString(),
+                Text = "Each 3 hours"
+            },
+            new ItemModel
+            {
+                ItemID = SyncBgTaskIntervals.EACH_6_HOURS.ToString(),
+                Text = "Each 6 hours"
+            },
+            new ItemModel
+            {
+                ItemID = SyncBgTaskIntervals.EACH_12_HOURS.ToString(),
+                Text = "Each 12 hours"
+            },
+            new ItemModel
+            {
+                ItemID = SyncBgTaskIntervals.EACH_24_HOURS.ToString(),
+                Text = "Each 24 hours"
+            }
+        };
         #endregion
 
-        #region Settings Properties
+
+        #region General Settings Properties
         public bool AskForPasswordWhenAppStarts
         {
             get => _appSettings.AskForPasswordWhenAppStarts;
-            set => _appSettings.AskForPasswordWhenAppStarts = value;
+            set
+            {
+                if (value)
+                {
+                    //_dialogService.ShowInputStringDialogAsync("")
+                }
+                _appSettings.AskForPasswordWhenAppStarts = value;
+            }
         }
 
-        public TaskSortType DefaultTaskSortOrder
+        public ItemModel CurrentTaskSortOrder
         {
-            get => _appSettings.DefaultTaskSortOrder;
-            set => _appSettings.DefaultTaskSortOrder = value;
+            get
+            {
+                var currentSelectedSortType = _appSettings.DefaultTaskSortOrder;
+                return TasksSortTypes.FirstOrDefault(i => i.ItemID == currentSelectedSortType.ToString());
+            }
+            set
+            {
+                var selectedSortType = (TaskSortType)Enum.Parse(typeof(TaskSortType), value.ItemID);
+                _appSettings.DefaultTaskSortOrder = selectedSortType;
+            }
         }
 
+        public bool ShowCompletedTasks
+        {
+            get => _appSettings.ShowCompletedTasks;
+            set => _appSettings.ShowCompletedTasks = value;
+        }
+        #endregion
+
+
+        #region Synchronization Settings Properties
+        public bool RunSyncBackgroundTaskAfterStart
+        {
+            get => _appSettings.RunSyncBackgroundTaskAfterStart;
+            set => _appSettings.RunSyncBackgroundTaskAfterStart = value;
+        }
+
+        public ItemModel CurrentSyncBackgroundTaskInterval
+        {
+            get
+            {
+                var currentInterval = _appSettings.SyncBackgroundTaskInterval;
+                return SyncBgTaskIntervalTypes.FirstOrDefault(s => s.ItemID == currentInterval.ToString());
+            }
+            set
+            {
+                var selectedInterval = (SyncBgTaskIntervals)Enum.Parse(typeof(SyncBgTaskIntervals), value.ItemID);
+                _appSettings.SyncBackgroundTaskInterval = selectedInterval;
+            }
+        }
+        #endregion
+
+
+        #region Notification Settings Properties
         public bool ShowToastNotificationAfterFullSync
         {
             get => _appSettings.ShowToastNotificationAfterFullSync;
             set => _appSettings.ShowToastNotificationAfterFullSync = value;
         }
 
-        public bool SyncBackgroundTaskAfterStart
+        public bool ShowToastNotificationForCompletedTasks
         {
-            get => _appSettings.SyncBackgroundTaskAfterStart;
-            set => _appSettings.SyncBackgroundTaskAfterStart = value;
-        }
-
-        public SyncBgTaskIntervals SyncBackgroundTaskInterval
-        {
-            get => _appSettings.SyncBackgroundTaskInterval;
-            set => _appSettings.SyncBackgroundTaskInterval = value;
+            get => _appSettings.ShowToastNotificationForCompletedTasks;
+            set => _appSettings.ShowToastNotificationForCompletedTasks = value;
         }
         #endregion
+
 
         #region Commands
         public ICommand NavigationRequestCommand { get; set; }
         #endregion
 
-        public SettingsPageViewModel(IApplicationSettingsService appSettings)
+        public SettingsPageViewModel(IApplicationSettingsService appSettings, ICustomDialogService dialogService)
         {
             _appSettings = appSettings;
 
