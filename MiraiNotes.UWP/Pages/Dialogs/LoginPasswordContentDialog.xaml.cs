95149f793d990f32205b29edd18e22ec298b3aed
Efrain Bastidas <ebastidas@smatersolutions.com.ve>
2018-09-16 18:13:45 -0400

    [UWP] Added a dialog to retrieve the password saved when the users logs in

---
+++ MiraiNotes.UWP/Pages/Dialogs/LoginPasswordContentDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MiraiNotes.UWP.ViewModels.Dialogs;
+using Windows.UI.Xaml.Controls;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MiraiNotes.UWP.Pages.Dialogs
+{
+    public sealed partial class LoginPasswordContentDialog : ContentDialog
+    {
+        private LoginPasswordDialogViewModel ViewModel => DataContext as LoginPasswordDialogViewModel;
+
+        public LoginPasswordContentDialog()
+        {
+            this.InitializeComponent();
+            this.PrimaryButtonClick += async (sender, args) =>
+            {
+                var deferral = args.GetDeferral();
+                bool matches = await ViewModel.PasswordMatches();
+
+                if (!matches)
+                    args.Cancel = true;
+
+                deferral.Complete();
+            };
+        }
+    }
+}
