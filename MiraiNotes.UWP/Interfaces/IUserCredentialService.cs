c745936e81a8b0f8d9b146b34293b71f8666df68
Efrain Bastidas <ebastidas@smatersolutions.com.ve>
2018-09-16 18:10:22 -0400

    [UWP] Added new methods that will be used in the credential service

--- MiraiNotes.UWP/Interfaces/IUserCredentialService.cs
+++ MiraiNotes.UWP/Interfaces/IUserCredentialService.cs
@@ -5,11 +5,16 @@ namespace MiraiNotes.UWP.Interfaces
 {
     public interface IUserCredentialService
     {
+        void SaveUserCredentials(PasswordVaultResourceType resource, string username, string password);
         void SaveUserCredentials(string resource, string username, string password);
         void SaveUserCredentials(string email, TokenResponse token);
         bool IsUserLoggedIn();
-        PasswordCredential GetUserCredentials(string resource);
+
+        string GetUserCredentials(PasswordVaultResourceType resource, string username);
+        //PasswordCredential GetUserCredentials(string resource);
         TokenResponse GetUserToken();
         void DeleteUserCredentials();
+
+        void DeleteUserCredentials(PasswordVaultResourceType resource, string username);
     }
 }
