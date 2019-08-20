fb756491a421a19e453fd278721319dba3d25f5b
Efrain Bastidas <mimo4325@gmail.com>
2019-08-20 00:14:36 -0500

    [Android] Added some icons, new layouts, new mvx msgs, new nugets

---
+++ MiraiNotes.Android/Common/Messages/TaskSavedMsg.cs
@@ -0,0 +1,13 @@
+﻿using MvvmCross.Plugin.Messenger;
+
+namespace MiraiNotes.Android.Common.Messages
+{
+    public class TaskSavedMsg : MvxMessage
+    {
+        public string TaskId { get; }
+        public TaskSavedMsg(object sender, string taskId) : base(sender)
+        {
+            TaskId = taskId;
+        }
+    }
+}
