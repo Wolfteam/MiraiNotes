9904f0052aa3a2b8b7cf1209ed71421f428da468
Efrain Bastidas <mimo4325@gmail.com>
2019-08-24 10:49:36 -0500

    [Core] Added initial notification models

---
+++ MiraiNotes.Core/Models/TaskNotification.cs
@@ -0,0 +1,10 @@
+﻿namespace MiraiNotes.Core.Models
+{
+    public class TaskNotification
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Conntent { get; set; }
+        public UwpNotificationSettings UwpSettings { get; set; }
+    }
+}
---
+++ MiraiNotes.Core/Models/TaskReminderNotification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MiraiNotes.Core.Models
+{
+    public class TaskReminderNotification
+    {
+        public int Id { get; set; }
+        public string TaskListId { get; set; }
+        public string TaskId { get; set; }
+        public string TaskListTitle { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskBody { get; set; }
+        public DateTimeOffset DeliveryOn { get; set; }
+    }
+}
