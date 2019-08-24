82e8e38c53904147d94156a84555d7bbfe1a2a54
Efrain Bastidas <mimo4325@gmail.com>
2019-08-24 10:49:50 -0500

    [Abstractions] Added a notification service

---
+++ MiraiNotes.Abstractions/Services/INotificationService.cs
@@ -0,0 +1,25 @@
+﻿using MiraiNotes.Core.Models;
+
+namespace MiraiNotes.Abstractions.Services
+{
+    public interface INotificationService
+    {
+        /// <summary>
+        /// Shows a simple toast notification with the title and the body specified
+        /// </summary>
+        /// <param name="notification">The notification options</param>
+        void ShowNotification(TaskNotification notification);
+
+        /// <summary>
+        /// Schedules a toast notification that serves as a reminder for a task
+        /// </summary>
+        /// <param name="notification">The notification options</param>
+        void ScheduleNotification(TaskReminderNotification notification);
+
+        /// <summary>
+        /// Removes a scheduled toast that matches the <paramref name="id"/>
+        /// </summary>
+        /// <param name="id">Id of the scheduled toast to remove</param>
+        void RemoveScheduledNotification(int id);
+    }
+}
