ba8ad6273dc992174331f779c8d0fddb585f95d6
Efrain Bastidas <mimo4325@gmail.com>
2019-08-30 10:37:11 -0500

    [Android] Added some converters

---
+++ MiraiNotes.Android/Converters/ImgPathToBitmapConverter.cs
@@ -0,0 +1,16 @@
+﻿using Android.Graphics;
+using MiraiNotes.Android.Common.Utils;
+using MvvmCross.Converters;
+using System;
+using System.Globalization;
+
+namespace MiraiNotes.Android.Converters
+{
+    public class ImgPathToBitmapConverter : MvxValueConverter<string, Bitmap>
+    {
+        protected override Bitmap Convert(string value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return MiscellaneousUtils.GetImageBitmap(value);
+        }
+    }
+}
---
+++ MiraiNotes.Android/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,14 @@
+﻿using MvvmCross.Converters;
+using System;
+using System.Globalization;
+
+namespace MiraiNotes.Android.Converters
+{
+    public class InverseBooleanConverter : MvxValueConverter<bool, bool>
+    {
+        protected override bool Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !value;
+        }
+    }
+}
