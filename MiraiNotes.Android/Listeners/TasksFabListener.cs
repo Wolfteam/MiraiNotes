a0702f5842ff430c72d0c0cc884defe566406b77
Efrain Bastidas <mimo4325@gmail.com>
2019-08-30 10:38:07 -0500

    [Android] Added a listener for the fab and progress overlay

---
+++ MiraiNotes.Android/Listeners/TasksFabListener.cs
@@ -0,0 +1,31 @@
+﻿using Android.Animation;
+using System;
+
+namespace MiraiNotes.Android.Listeners
+{
+    public class TasksFabListener : Java.Lang.Object, Animator.IAnimatorListener
+    {
+        private Action _onAnimationEnd;
+        public TasksFabListener(Action onAnimationEnd)
+        {
+            _onAnimationEnd = onAnimationEnd;
+        }
+
+        public void OnAnimationCancel(Animator animation)
+        {
+        }
+
+        public void OnAnimationEnd(Animator animation)
+        {
+            _onAnimationEnd();
+        }
+
+        public void OnAnimationRepeat(Animator animation)
+        {
+        }
+
+        public void OnAnimationStart(Animator animation)
+        {
+        }
+    }
+}
