018f413884c0fd4073b96037e5d960bd97f1ef9b
Efrain Bastidas <mimo4325@gmail.com>
2019-09-02 23:11:28 -0500

    [Shared] Added app accent colors to a constant. Added a color util

--- MiraiNotes.Shared/AppConstants.cs
+++ MiraiNotes.Shared/AppConstants.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
 namespace MiraiNotes.Shared
 {
     public static class AppConstants
@@ -8,12 +11,21 @@ namespace MiraiNotes.Shared
 
 #if Android
         public const string ClientId = "xxxx";
-        public const string ClientSecret = "";
+        public const string ClientSecret = "xxxx";
         public const string RedirectUrl = "xxxx";
 #else
         public const string ClientId = "xxxx";
         public const string ClientSecret = "xxxx";
         public const string RedirectUrl = "xxxx";
 #endif
+
+        public static IReadOnlyList<string> AppAccentColors => new List<string>
+        {
+            "#0077dd", "#008888", "#ee0088",
+            "#cc4400", "#ee1122","#008899",
+            "#118833","#881199", "#cc33bb",
+            "#777777", "#ffb900", "#ff8c00",
+            "#0063b1", "#6b69d6", "#68768a"
+        };
     }
 }
