905fec681a65fb90a64ca15b762610432c19d144
Efrain Bastidas <mimo4325@gmail.com>
2019-09-16 21:27:19 -0500

    [Core] Added more values to the sync bg enum

--- MiraiNotes.Core/Enums/SyncBgTaskIntervals.cs
+++ MiraiNotes.Core/Enums/SyncBgTaskIntervals.cs
@@ -7,6 +7,8 @@
     public enum SyncBgTaskIntervals
     {
         NEVER = 0,
+        EACH_30_MIN = 30,
+        EACH_1_HOUR = 60,
         EACH_3_HOURS = 180,
         EACH_6_HOURS = 360,
         EACH_12_HOURS = 720,
