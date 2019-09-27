91c312f8755c4a89dde18b3646959f773da08e7f
Efrain Bastidas <mimo4325@gmail.com>
2019-09-26 23:00:52 -0500

    [Android] Added a binding that will show the subtasks and another one that sets a strike through text decoration

---
+++ MiraiNotes.Android/Bindings/StrikeThroughTextBinding.cs
@@ -0,0 +1,28 @@
+﻿using Android.Graphics;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Binding.Target;
+using System;
+
+namespace MiraiNotes.Android.Bindings
+{
+    public class StrikeThroughTextBinding : MvxAndroidTargetBinding
+    {
+        public static string PropertyName => "StrikeThroughText";
+        public override Type TargetType => typeof(bool);
+
+        public StrikeThroughTextBinding(object target) : base(target)
+        {
+        }
+
+        protected override void SetValueImpl(object target, object value)
+        {
+            var textView = (TextView)target;
+            bool strikeThrough = (bool)value;
+
+            if (strikeThrough)
+                textView.PaintFlags |= PaintFlags.StrikeThruText;
+            else
+                textView.PaintFlags &= ~PaintFlags.StrikeThruText;
+        }
+    }
+}
