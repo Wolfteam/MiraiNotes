a7bb4626185fcb0df54f8ecefcb1de5ec69c6acc
Efrain Bastidas <mimo4325@gmail.com>
2019-09-26 23:09:39 -0500

    [Android] Added a custom adapter for the tasks view

---
+++ MiraiNotes.Android/Adapters/TasksAdapter.cs
@@ -0,0 +1,88 @@
+﻿using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Support.V4.View;
+using Android.Support.V7.Widget;
+using Android.Views;
+using MiraiNotes.Android.Interfaces;
+using MiraiNotes.Android.Listeners;
+using MiraiNotes.Android.ViewModels;
+using MvvmCross.Binding.Extensions;
+using MvvmCross.Droid.Support.V7.RecyclerView;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
+using MvvmCross.ViewModels;
+using System;
+using System.Linq;
+
+namespace MiraiNotes.Android.Adapters
+{
+    public class TasksAdapter : MvxRecyclerAdapter, IItemTouchHelperAdapter
+    {
+        public TasksAdapter(IMvxAndroidBindingContext bindingContext)
+            : base(bindingContext)
+        {
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            var itemBindingContext = new MvxAndroidBindingContext(parent.Context, BindingContext.LayoutInflaterHolder);
+            var view = InflateViewForHolder(parent, viewType, itemBindingContext);
+
+            return new TaskViewHolder(view, itemBindingContext);
+            //var view = BindingContext.BindingInflate(Resource.Layout.TaskItem, parent);
+            //var itemViewHolder = new TaskViewHolder(view, BindingContext);
+            //return itemViewHolder;
+        }
+
+        public void OnItemSwiped(int position, bool swipedToTheRight)
+        {
+            var items = (MvxObservableCollection<TaskItemViewModel>)ItemsSource;
+            var item = items.ElementAt(position);
+
+            if (!swipedToTheRight)
+            {
+                item.DeleteTaskCommand.Execute();
+                //items.Remove(item);
+                //NotifyDataSetChanged();
+                //NotifyItemRemoved(position);
+                //NotifyItemRangeChanged(position, ItemCount - position);
+            }
+            else
+            {
+                item.ChangeTaskStatusCommand.Execute();
+                items.Remove(item);
+                items.Add(item);
+            }
+        }
+
+        public bool OnItemMove(int fromPosition, int toPosition)
+        {
+            var items = (MvxObservableCollection<TaskItemViewModel>)ItemsSource;
+            items.Move(fromPosition, toPosition);
+            NotifyItemMoved(fromPosition, toPosition);
+            return true;
+        }
+
+        public class TaskViewHolder : MvxRecyclerViewHolder, IItemTouchHelperViewHolder
+        {
+            private readonly View _itemView;
+            private readonly Drawable _initialColor;
+
+            public TaskViewHolder(View itemView, IMvxAndroidBindingContext context)
+                : base(itemView, context)
+            {
+                _itemView = itemView;
+                _initialColor = _itemView.Background;
+            }
+
+            public void OnItemSelected()
+            {
+                _itemView.SetBackgroundColor(Color.LightGray);
+            }
+
+            public void OnItemClear()
+            {
+                ViewCompat.SetBackground(_itemView, _initialColor);
+            }
+        }
+    }
+}
---
+++ MiraiNotes.Android/Interfaces/IOnStartDragListener.cs
@@ -0,0 +1,16 @@
+﻿using Android.Support.V7.Widget;
+
+namespace MiraiNotes.Android.Interfaces
+{
+    /// <summary>
+    /// Listener for manual initiation of a drag.
+    /// </summary>
+    public interface IOnStartDragListener
+    {
+        /// <summary>
+        /// Called when a view is requesting a start of a drag.
+        /// </summary>
+        /// <param name="viewHolder">The holder of the view to drag.</param>
+        void OnStartDrag(RecyclerView.ViewHolder viewHolder);
+    }
+}
